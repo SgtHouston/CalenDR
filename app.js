9f1f26469f0c49db64a20e57b91659a90d50ef00
Chris Houston <chrisbcoding247@gmail.com>
2021-07-27 12:49:55 -0400

    added sequelize

--- app.js
+++ app.js
@@ -3,6 +3,30 @@
 // npm install 
 // git init
 // .gitignore
+// npm install sequelize
+// install postgres package -> npm install pg
+// npm install sequelize-cli 
+// npx sequelize-cli init
+// Development: username and dialect changed to postgres in config.json
+// Development:database = project name 
+// npx sequelize-cli db:create to create database in Postgres
+// npx sequelize-cli model:generate --name Patient \--attributes "name:string, birthdate:dateonly, covid:boolean, phone:string"
+// npx sequelize-cli db:migrate
+// npx sequelize-cli model:generate --name Doctor \--attributes "name:string, specialty:string, location:string"
+// npx sequelize-cli model:generate --name Appointment --attributes "time:date, DoctorId:integer, PatientId:integer"
+// Edit migration files to link databases
+// references: {
+//     model: 'Patients',
+//     key: 'id'
+//   } ties tables together using key
+// references: {
+//     model: 'Doctors',
+//     key: 'id'
+//   }
+// define associations in appointment.js/doctor.js/patient.js
+// // // define association here
+// Appointment.belongsTo(models.Patient)
+// Appointment.belongsTo(models.Doctor)
 
 var express = require('express');
 var path = require('path');
